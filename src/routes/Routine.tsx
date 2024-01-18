a8073ab073493a540d1831ceb819d1b5d21a6713
Malwine <malwine@mailbox.org>
2024-01-18 20:52:13 +0100

    Start and end an iteration

--- src/routes/Routine.tsx
+++ src/routes/Routine.tsx
@@ -6,25 +6,55 @@ import { formatDate } from '../lib/dateConversion';
 
 interface Iteration {
   start: string;
-}
+  running?: boolean;
+  end?: string;
+};
 
 interface RoutineData {
   name: string;
   iterations?: Iteration[];
-}
+};
 
 export async function loader({ params }: {params: Params}) {
   return jsonData.routines.find(routine => routine.name === params.routine);
-}
+};
 
 const Routine = () => {
   const routine = useLoaderData() as RoutineData;
+  
+
+  const handleOnStart = (routine: RoutineData) => {
+    if(routine.iterations) {
+      let runningRoutine = routine.iterations.find(iteration => iteration.running) || false;
+  
+      if (!runningRoutine) {
+        let currentDate = new Date();
+        let newRoutine = {"start": currentDate.toISOString(), "running": true};
+        routine.iterations.push(newRoutine)
+
+      }
+      console.log("start clicked", routine.iterations)
+    } 
+  };
+  
+  const handleOnEnd = (routine: RoutineData) => {
+    if(routine.iterations) {
+      let runningRoutine = routine.iterations.find(iteration => iteration.running) || false;
+  
+      if (runningRoutine) {
+        runningRoutine.running = false;
+        let currentDate = new Date();
+        runningRoutine.end = currentDate.toISOString();
+      }
+      console.log("end clicked", routine.iterations)
+    }
+  };
 
   return (
     <div>
       <h1>{ routine.name }</h1>
-        <Button onClick={()=> {}}>Start</Button>
-        <Button onClick={()=> {}}>End</Button>
+        <Button onClick={() => handleOnStart(routine)}>Start</Button>
+        <Button onClick={() => handleOnEnd(routine)}>End</Button>
 
       <h3>Past iterations:</h3>
 
